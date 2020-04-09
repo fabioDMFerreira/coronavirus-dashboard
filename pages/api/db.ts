d2273418e11b61042661428d7eb9f3493f04d64c
Fábio Ferreira <martinhoferreira10@gmail.com>
2020-04-09 10:53:34 +0100

    add docker files

--- pages/api/db.ts
+++ pages/api/db.ts
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
+console.log('mongo uri', process.env.MONGO_URI)
 if (mongoose.connection.readyState === 0) {
-  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .catch(err => { throw err })
 }
 
 export default {
