053244318c6739f9bc9f67306938350fb7718c4d
Fábio Ferreira <martinhoferreira10@gmail.com>
2020-04-09 10:32:22 +0100

    use environment variable on connecting to mongodb

--- pages/api/db.ts
+++ pages/api/db.ts
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 if (mongoose.connection.readyState === 0) {
-  mongoose.connect('mongodb://root:password@localhost:27017/coronavirus', { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 }
 
 export default {
